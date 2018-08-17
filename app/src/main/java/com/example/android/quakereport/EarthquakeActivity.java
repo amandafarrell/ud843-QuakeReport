c6ae47cdcd42acff5abdf2e61885ada66fbdfea4
Amanda Farrell <bluepandadev@gmail.com>
2018-08-17 10:27:17 -0600

    Replaced AsyncTask wit hAsyncTaskLoader

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,9 +15,10 @@
  */
 package com.example.android.quakereport;
 
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -25,10 +26,17 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Earthquake>>{
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    /**
+     * Constant value for the earthquake loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int EARTHQUAKE_LOADER_ID = 1;
 
     /** URL for earthquake data from the USGS dataset */
     private static final String USGS_REQUEST_URL =
@@ -67,49 +75,35 @@ public class EarthquakeActivity extends AppCompatActivity {
             }
         });
 
-        // Start the AsyncTask to fetch the earthquake data
-        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
-        task.execute(USGS_REQUEST_URL);
+        //Get a reference to the LoaderManager, in order to interact with loaders.
+        LoaderManager loaderManager = getLoaderManager();
+
+        //Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        //the bundle. Pass in this activity for LoaderCallbacks parameter (which is valid
+        //because this activity implements the LoaderCallbacks interface).
+        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
     }
 
-    /**
-     * {@link AsyncTask} to perform the network request on a background thread, and then
-     * update the UI with the list of earthquakes in the response.
-     *
-     * AsyncTask has three generic parameters: the input type, a type used for progress updates, and
-     * an output type. Our task will take a String URL, and return an Earthquake. We won't do
-     * progress updates, so the second generic is just Void.
-     *
-     * We'll only override two of the methods of AsyncTask: doInBackground() and onPostExecute().
-     * The doInBackground() method runs on a background thread, so it can run long-running code
-     * (like network activity), without interfering with the responsiveness of the app.
-     * Then onPostExecute() is passed the result of doInBackground() method, but runs on the
-     * UI thread, so it can use the produced data to update the UI.
-     */
-    private class EarthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<Earthquake>>{
-        @Override
-        protected ArrayList<Earthquake> doInBackground(String... strings) {
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
+        return new EarthquakeLoader(this,USGS_REQUEST_URL);
+    }
 
-            // Perform the HTTP request for earthquake data and process the response.
-            if (strings.length < 1 || strings[0] == null){
-                return null;
-            }
+    @Override
+    public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> earthquakes) {
+        //Clear the adapter of previous earthquake data
+        mAdapter.clear();
 
-            ArrayList<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(strings[0]);
-            return earthquakes;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Earthquake> earthquakes) {
-            //Clear the adapter of previous earthquake data
-            mAdapter.clear();
-
-            //If there is a valid list of {@link Earthquake}s then add them to the adapter's
-            //data set. This will trigger the ListView to update
-            if (earthquakes != null && !earthquakes.isEmpty()){
-                mAdapter.addAll(earthquakes);
-            }
+        //If there is a valid list of {@link Earthquake}s then add them to the adapter's
+        //data set. This will trigger the ListView to update
+        if (earthquakes != null && !earthquakes.isEmpty()){
+            mAdapter.addAll(earthquakes);
         }
     }
 
+    @Override
+    public void onLoaderReset(Loader<List<Earthquake>> loader) {
+        //Loader reset, so we can clear out our existing data
+        mAdapter.clear();
+    }
 }
---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -0,0 +1,48 @@
+package com.example.android.quakereport;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Loads a list of earthquakes by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link EarthquakeLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public EarthquakeLoader (Context context, String url){
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<Earthquake> loadInBackground() {
+        // Perform the HTTP request for earthquake data and process the response.
+        if (mUrl == null){
+            return null;
+        }
+
+        ArrayList<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(mUrl);
+        return earthquakes;
+    }
+}
+
